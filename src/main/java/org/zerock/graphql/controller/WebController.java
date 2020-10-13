a53a42d9d4277392abda3691c8edc34524e54bbd
DESKTOP-QFGCIDP\LDY <dlaehdud96@naver.com>
2020-10-13 20:07:13 +0900

    1

--- src/main/java/org/zerock/graphql/controller/WebController.java
+++ src/main/java/org/zerock/graphql/controller/WebController.java
@@ -32,6 +32,37 @@ public class WebController {
 	    model.addAttribute("paginationProducts", result);
 		return "productList";
 	}*/
+	@GetMapping("/restaurant/1k5A/menus") 
+	public String menulist() {
+		
+		HashMap<String, Object> result = new HashMap<String, Object>();
+		String jsonInString = "";
+		
+		 try {
+	            RestTemplate restTemplate = new RestTemplate();
+	 
+	            HttpHeaders header = new HttpHeaders();
+	            HttpEntity<?> entity = new HttpEntity<>(header);
+	 
+	            String url = "http://ec2-3-34-50-187.ap-northeast-2.compute.amazonaws.com:4000";
+	      
+	            //이 한줄의 코드로 API를 호출해 MAP타입으로 전달 받는다.
+	            jsonInString = restTemplate.getForObject(url + "/restaurant/1k5A/menus", String.class);
+	 
+	        } catch (HttpClientErrorException | HttpServerErrorException e) {
+	            result.put("statusCode", e.getRawStatusCode());
+	            result.put("body"  , e.getStatusText());
+	            System.out.println("dfdfdfdf");
+	            System.out.println(e.toString());
+	 
+	        } catch (Exception e) {
+	            result.put("statusCode", "999");
+	            result.put("body"  , "excpetion오류");
+	            System.out.println(e.toString());
+	        }
+	 
+	        return jsonInString;
+	}
 	
 	@GetMapping("/kinds/k5/restaurants") 
 	public String kindlist() {
@@ -63,6 +94,52 @@ public class WebController {
 	        }
 	 
 	        return jsonInString;
+	}
+	
+	@GetMapping("/menulist")
+	public String callAPI2() {
+		
+		HashMap<String, Object> result = new HashMap<String, Object>();
+		 
+        String jsonInString = "";
+        
+        try {
+           
+            RestTemplate restTemplate = new RestTemplate();
+ 
+            HttpHeaders header = new HttpHeaders();
+            HttpEntity<?> entity = new HttpEntity<>(header);
+ 
+            String url = "http://ec2-3-34-50-187.ap-northeast-2.compute.amazonaws.com:4000";
+            
+            final String body = "{\r\n" + 
+            		"  kind(kind_code: \"k5\"){\r\n" + 
+            		"    restaurants{\r\n" + 
+            		"      company_code\r\n" + 
+            		"    	member_id\r\n" + 
+            		"    	restaurant_address\r\n" + 
+            		"   	 	restaurant_name\r\n" + 
+            		"    	restaurant_phone\r\n" + 
+            		"    }\r\n" + 
+            		"  }\r\n" + 
+            		"}";
+      
+            //이 한줄의 코드로 API를 호출해 MAP타입으로 전달 받는다.
+            jsonInString = restTemplate.getForObject(url + "/graphql?query={body}", String.class,body);
+ 
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            result.put("statusCode", e.getRawStatusCode());
+            result.put("body"  , e.getStatusText());
+            System.out.println("dfdfdfdf");
+            System.out.println(e.toString());
+ 
+        } catch (Exception e) {
+            result.put("statusCode", "999");
+            result.put("body"  , "excpetion오류");
+            System.out.println(e.toString());
+        }
+ 
+        return jsonInString;
 	}
 	
 	@GetMapping("/kindlist")
