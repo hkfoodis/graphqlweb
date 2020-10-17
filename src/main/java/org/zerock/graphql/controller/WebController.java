bb52f0e662cb6519ce1ca03c4b71277125c5c5ab
DESKTOP-QFGCIDP\LDY <dlaehdud96@naver.com>
2020-10-17 20:49:20 +0900

    1

--- src/main/java/org/zerock/graphql/controller/WebController.java
+++ src/main/java/org/zerock/graphql/controller/WebController.java
@@ -203,6 +203,56 @@ public class WebController {
  
         return jsonInString;
 	}
+	@GetMapping("/kindlistk51")
+	public String callAPI6() {
+		
+		HashMap<String, Object> result = new HashMap<String, Object>();
+		 
+        String jsonInString = "";
+        
+        try {
+           
+            RestTemplate restTemplate = new RestTemplate();
+ 
+            HttpHeaders header = new HttpHeaders();
+            HttpEntity<?> entity = new HttpEntity<>(header);
+ 
+            String url = "http://ec2-3-34-50-187.ap-northeast-2.compute.amazonaws.com:4000";
+            
+            final String body = "{\r\n" + 
+            		"  kind(kind_code: \"k5\"){\r\n" + 
+            		"    kind_code\r\n" + 
+            		"    restaurants{\r\n" + 
+            		"      company_code\r\n" + 
+            		"      member_id\r\n" + 
+            		"      kind_code\r\n" + 
+            		"      restaurant_name\r\n" + 
+            		"      restaurant_address\r\n" + 
+            		"      restaurant_phone\r\n" + 
+            		"      restaurant_img\r\n" + 
+            		"      board_comment_code\r\n" + 
+            		"      star_avg_score\r\n" + 
+            		"    }\r\n" + 
+            		"  }\r\n" + 
+            		"}";
+      
+            //이 한줄의 코드로 API를 호출해 MAP타입으로 전달 받는다.
+            jsonInString = restTemplate.getForObject(url + "/graphql?query={body}", String.class,body);
+ 
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            result.put("statusCode", e.getRawStatusCode());
+            result.put("body"  , e.getStatusText());
+            System.out.println("dfdfdfdf");
+            System.out.println(e.toString());
+ 
+        } catch (Exception e) {
+            result.put("statusCode", "999");
+            result.put("body"  , "excpetion오류");
+            System.out.println(e.toString());
+        }
+ 
+        return jsonInString;
+	}
 	
 	@GetMapping("/kindlistk5")
 	public String callAPI() {
